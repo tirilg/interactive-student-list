edf80a1363863656d6f03d16d4ecf43dc1c5f06c
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-09-12 15:32:57 +0200

    names split into first and last names

--- script.js
+++ script.js
@@ -1,21 +1,70 @@
 //"use strict";
 
-let template = document.querySelector("#studentTemp").content;
+document.addEventListener("DOMContentLoaded", fetchStudents);
 
+let template = document.querySelector("#temp").content;
+
+let students = [];
 //fetch
 function fetchStudents() {
   fetch("studentlist.json")
-    .then(e => e.json())
-    .then(showStudents);
+    .then(result => result.json())
+    .then(createList);
 }
 
-function showStudents(data) {
-  console.log(data);
-  data.forEach(showSingleStudent);
+function createList(data) {
+  const names = Object.values(data);
+  console.log(names);
+
+  data.forEach(person => {
+    let temp = Object.create(studentTemp);
+    temp.splitName(person);
+    students.push(temp);
+  });
+
+  const byFirstName = students.sort(sortByFirstName); //sort students by first name
+  show(byFirstName, "#studentName"); //run function show(), which displays the sorted list by first name
 }
 
-function showSingleStudent(aStudent) {
-  let clone = template.cloneNode(true);
+function sortByFirstName(a, b) {
+  if (a.firstName < b.firstName) {
+    //if value of a.firstName is smaller than value of b.firstName (in this case the value of the letter), then move the name up (value of letters is smallest at A and biggest at Z)
+    return -1; //means move it up
+  } else {
+    return 1; //means move it down
+  }
+}
+
+//function that sorts by last name by seeing if a (the first object) is smaller than b(the second object) (watch robot video)
+function sortByLastName(a, b) {
+  if (a.lastName < b.lastName) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+function show(data, selector) {
+  data.forEach(person => {
+    const li = document.createElement("li"); //creates listed items for each of the persons
+    li.textContent = person; //adds text content from person into the li (the names and house)
+    document.querySelector(selector).appendChild(li); //append the data into #first and #last in html
+  });
 }
 
 fetchStudents();
+
+const studentTemp = {
+  firstName: "", //insert first name
+  lastName: "", //insert last name
+  toString() {
+    //converts to string and adds the objects of what it is running through
+    return this.firstName + " " + this.lastName;
+  },
+  splitName(fullName) {
+    //splits the name into first and last
+    const firstSpace = fullName.indexOf(" ");
+    this.firstName = fullName.substring(0, firstSpace);
+    this.lastName = fullName.substring(firstSpace + 1);
+  }
+};
