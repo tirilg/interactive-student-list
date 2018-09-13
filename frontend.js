125056dbc2d0cd9b13dda94b379742097b657214
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-09-13 18:27:10 +0200

    student list displays and sorting last and first name works

---
+++ frontend.js
@@ -0,0 +1,44 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", initFrontend);
+
+function initFrontend() {
+  console.log("frontend is running");
+
+  //register buttons for sort
+  document
+    .querySelector("button#sort_first")
+    .addEventListener("click", clickedSortFirstname);
+  document
+    .querySelector("button#sort_last")
+    .addEventListener("click", clickedSortLastname);
+}
+
+function clickedSortFirstname() {
+  console.log("clickedSortFirstname");
+  sortByFirstName();
+  displayList(students);
+}
+
+function clickedSortLastname() {
+  console.log("clickedSortLastname");
+  sortByLastName();
+  displayList(students);
+}
+
+function displayList(listOfStudents) {
+  document.querySelector("table#student_list tbody").innerHTML = "";
+  listOfStudents.forEach(function(student) {
+    const clone = document
+      .querySelector("#student_template")
+      .content.cloneNode(true);
+
+    //fill clone with data
+    clone.querySelector("[data-firstname]").textContent = student.firstName;
+    clone.querySelector("[data-middlename]").textContent = student.middleName;
+    clone.querySelector("[data-lastname]").textContent = student.lastName;
+
+    //append clone to table
+    document.querySelector("table#student_list tbody").appendChild(clone);
+  });
+}
--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-//"use strict";
-
-//let template = document.querySelector("#temp").content;
+"use strict";
 
 let students = [];
+
 //fetch
 function fetchStudents() {
   fetch("studentlist.json")
@@ -14,47 +13,75 @@ function createList(data) {
   const names = Object.values(data);
   //console.log(names);
 
+  const studentTemp = {
+    firstName: "", //insert first name
+    middleName: "", //insert middle name
+    lastName: "", //insert last name
+    toString() {
+      //converts to string and adds the objects of what it is running through
+      return this.firstName + " " + this.middleName + " " + this.lastName;
+    },
+    splitName(fullName) {
+      //splits the name into first and last
+      const firstSpace = fullName.indexOf(" ");
+      const lastSpace = fullName.lastIndexOf(" ");
+      this.firstName = fullName.substring(0, firstSpace);
+      this.middleName = fullName.substring(firstSpace + 1, lastSpace);
+      this.lastName = fullName.substring(lastSpace + 1);
+    }
+  };
+
   data.forEach(person => {
     let temp = Object.create(studentTemp);
     temp.splitName(person);
     students.push(temp);
   });
 
+  displayList(students);
   console.table(students);
-
-  const byFirstName = students.sort(sortByFirstName); //sort students by first name
-  show(byFirstName, "#studentName"); //run function show(), which displays the sorted list by first name
+  //const byFirstName = students.sort(sortByFirstName); //sort students by first name //add event to a button to sort
 }
 
-function sortByFirstName(a, b) {
-  if (a.firstName < b.firstName) {
-    //if value of a.firstName is smaller than value of b.firstName (in this case the value of the letter), then move the name up (value of letters is smallest at A and biggest at Z)
-    return -1; //means move it up
-  } else {
-    return 1; //means move it down
+function sortByFirstName() {
+  //function that sorts by first name by seeing if a (the first object) is smaller than b(the second object) (watch robot video)
+  function byFirstName(a, b) {
+    if (a.firstName < b.firstName) {
+      //if value of a.firstName is smaller than value of b.firstName (in this case the value of the letter), then move the name up (value of letters is smallest at A and biggest at Z)
+      return -1; //means move it up
+    } else {
+      return 1; //means move it down
+    }
   }
+  students.sort(byFirstName);
 }
 
-//function that sorts by last name by seeing if a (the first object) is smaller than b(the second object) (watch robot video)
-function sortByLastName(a, b) {
-  if (a.lastName < b.lastName) {
-    return -1;
-  } else {
-    return 1;
+function sortByMiddleName() {
+  //function that sorts by last name by seeing if a (the first object) is smaller than b(the second object) (watch robot video)
+  function byMiddleName(a, b) {
+    if (a.middleName < b.middleName) {
+      return -1;
+    } else {
+      return 1;
+    }
   }
+  students.sort(byMiddleName);
 }
 
-function show(data, selector) {
-  data.forEach(person => {
-    const h1 = document.createElement("h1"); //creates listed items for each of the persons
-    h1.textContent = person; //adds text content from person into the li (the names and house)
-    document.querySelector(selector).appendChild(h1); //append the data into #first and #last in html
-  });
+function sortByLastName() {
+  //function that sorts by last name by seeing if a (the first object) is smaller than b(the second object) (watch robot video)
+  function byLastName(a, b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
+  students.sort(byLastName);
 }
 
 fetchStudents();
 
-const studentTemp = {
+/*const studentTemp = {
   firstName: "", //insert first name
   lastName: "", //insert last name
   toString() {
@@ -64,7 +91,9 @@ const studentTemp = {
   splitName(fullName) {
     //splits the name into first and last
     const firstSpace = fullName.indexOf(" ");
+    const lastSpace = fullName.lastIndexOf(" ");
     this.firstName = fullName.substring(0, firstSpace);
-    this.lastName = fullName.substring(firstSpace + 1);
+    this.middleName = fullName.substring(firstSpace + 1, lastSpace);
+    this.lastName = fullName.substring(lastSpace + 1);
   }
-};
+};*/
